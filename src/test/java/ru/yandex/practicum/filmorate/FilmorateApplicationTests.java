00840307bc47f75fa12c9ff29acf29d5eb96e883
42motat <maxotatarnikov@yandex.ru>
2025-10-07 02:20:58 +0800

    выполнение ТЗ для ФЗ12. добавление работы с БД

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,14 +1,14 @@
-package ru.yandex.practicum.filmorate;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class FilmorateApplicationTests {
-
-    @Test
-    void contextLoads() {
-
-    }
-
-}
+//package ru.yandex.practicum.filmorate;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class FilmorateApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//
+//    }
+//
+//}
